3630dbe8d335f7d157eb3257784536d8fe56c9bf
paultgoodchild <paultgoodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-03 12:11:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-bootstrap-css/trunk@581316 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/worpit_wtb_index.php
+++ views/worpit_wtb_index.php
@@ -31,8 +31,8 @@
 				<p>To learn more about what shortcodes are, <a href="http://www.hostliketoast.com/2011/12/how-extend-wordpress-powerful-shortcodes/">check this link</a></p>
 				<p>The following shortcodes are available:</p>
 				<ol>
-					<li>[ TBS_ROW ] <span class="label label-success">new</span></li>
-					<li>[ TBS_SPAN ] <span class="label label-success">new</span></li>
+					<li>[ <a href="http://bit.ly/T65cho" title="Twitter Bootstrap Row Shortcode">TBS_ROW</a> ] <span class="label label-success">new</span></li>
+					<li>[ <a href="http://bit.ly/T65cho" title="Twitter Bootstrap Span Shortcode">TBS_SPAN</a> ] <span class="label label-success">new</span></li>
 					<li>[ <a href="http://bit.ly/HOt01C" title="Twitter Bootstrap Badge Shortcode">TBS_BADGE</a> ] </li>
 					<li>[ <a href="http://bit.ly/zmGUeD" title="Twitter Bootstrap Glyph Icon WordPress Shortcode">TBS_ICON</a> ]</li>
 					<li>[ <a href="http://bit.ly/AlETMx" title="Twitter Bootstrap Button WordPress Shortcode">TBS_BUTTON</a> ]</li>
